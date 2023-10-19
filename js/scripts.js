32093770d1a639c78a5ed44fac673e65341d0758
Dakota Ponder <dponder12358@gmail.com>
2023-10-19 13:15:20 -0500

    added content

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Sample initialization code for a library like OwlCarousel
+$(document).ready(function(){
+    $("#testimonialCarousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000
+    });
+});
