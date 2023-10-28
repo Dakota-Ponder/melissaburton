102994caa722327635572f79d5e1bfd0edb06262
Dakota Ponder <dponder12358@gmail.com>
2023-10-28 13:58:38 -0500

    changed a lot

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Sample initialization code for a library like OwlCarousel
-$(document).ready(function(){
-    $("#testimonialCarousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 5000
-    });
-});
